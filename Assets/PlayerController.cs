6c85132f933d2660e49808201c659dd76da06c35
Denis Pyš <denis.pys@seznam.cz>
2024-10-19 10:42:19 +0200

    wip555

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,41 +1,52 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Rychlost pohybu
-    private Rigidbody2D rb; // Reference na Rigidbody2D
-    private Animator animator; // Reference na Animator
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;  // Odkaz na Animator
 
-    private Vector2 moveInput; // Vstup pro pohyb
-    
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-
-
-
-    }
+    private Vector2 moveInput;
+    private bool isKicking = false;
 
     void Update()
     {
-        // Získání horizontálního a vertikálního vstupu
-        moveInput.x = Input.GetAxis("Horizontal");
-        moveInput.y = Input.GetAxis("Vertical");
+        // Získání vstupu
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
 
-        // Nastavení parametru Speed v Animatoru
+        // Nastavení animací pro pohyb (animátor aktualizuje pohyb na základě směru a rychlosti)
         animator.SetFloat("Horizontal", moveInput.x);
         animator.SetFloat("Vertical", moveInput.y);
-        animator.SetFloat("Speed", moveInput.magnitude); // Nastavení rychlosti
-        
+        animator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+        // Kontrola kopání
+        if (Input.GetKeyDown(KeyCode.E) && !isKicking)
+        {
+            isKicking = true;
+            animator.SetBool("isKicking", true);  // Nastavení proměnné v animátoru
+            TriggerKick();
+        }
     }
 
     void FixedUpdate()
     {
-        // Pohyb pouze pokud existuje vstup
-        if (moveInput != Vector2.zero)
+        // Pohyb pouze pokud postava nekopá
+        if (!isKicking)
         {
             rb.MovePosition(rb.position + moveInput * (moveSpeed * Time.fixedDeltaTime));
         }
+    }
+
+    void TriggerKick()
+    {
+        // Nastavení zpoždění kopání
+        Invoke("StopKicking", 0.5f);  // Časový úsek pro kopnutí (např. 0.5s)
+    }
+
+    void StopKicking()
+    {
+        isKicking = false;
+        animator.SetBool("isKicking", false);  // Zpět na false, když kopání skončí
     }
 }
